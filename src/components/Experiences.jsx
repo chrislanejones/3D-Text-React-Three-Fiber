5a8d87c839f1afb4de575d4856edc252b78da24f
Chris Lane Jones <chrislanejones@gmail.com>
2024-06-18 03:25:38 -0400

    Tent Added - Tent Seperated from Graphic

--- src/components/Experiences.jsx
+++ src/components/Experiences.jsx
@@ -1,13 +1,27 @@
-import { OrbitControls } from "@react-three/drei";
+import { Environment, OrbitControls, Text } from "@react-three/drei";
+import * as THREE from "three";
+import { Camping } from "./Camping";
+import { degToRad } from "three/src/math/MathUtils.js";
 
 export const Experience = () => {
   return (
     <>
       <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+      <Text
+        font="fonts/NotoSans_Condensed-ExtraBold.ttf"
+        position-x={-1.3}
+        position-y={-0.5}
+        position-z={1}
+        lineHeight={0.8}
+        textAlign={"center"}
+      >
+        My Little{"\n"} Camping
+        <meshBasicMaterial color="white" />
+      </Text>
+      <group rotateY={degToRad(-25)}>
+        <Camping scale={0.6} />
+      </group>
+      <Environment preset="sunset" />
     </>
   );
 };
