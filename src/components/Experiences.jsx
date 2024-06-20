3ee7afd59917d2269bcfbefce999e49d292b99ac
Chris Lane Jones <chrislanejones@gmail.com>
2024-06-19 23:25:44 -0400

    CameraControls - Yomotsu - Dolly

--- src/components/Experiences.jsx
+++ src/components/Experiences.jsx
@@ -1,12 +1,28 @@
-import { Environment, OrbitControls, Text } from "@react-three/drei";
+import {
+  CameraControls,
+  Environment,
+  OrbitControls,
+  Text,
+} from "@react-three/drei";
 import * as THREE from "three";
 import { Camping } from "./Camping";
 import { degToRad } from "three/src/math/MathUtils.js";
+import { useEffect, useRef } from "react";
 
 export const Experience = () => {
+  const controls = useRef();
+
+  const intro = async () => {
+    controls.current.dolly(-22);
+  };
+
+  useEffect(() => {
+    intro();
+  }, []);
+
   return (
     <>
-      <OrbitControls />
+      <CameraControls ref={controls} />
       <Text
         font="fonts/NotoSans_Condensed-ExtraBold.ttf"
         position-x={-1.3}
