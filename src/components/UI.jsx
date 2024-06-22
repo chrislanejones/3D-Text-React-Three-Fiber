141f48a840eb5070b4048adbdb29bf0aa455ebe6
Chris Lane Jones <chrislanejones@gmail.com>
2024-06-21 23:59:47 -0400

    Jotai Addon

--- src/components/UI.jsx
+++ src/components/UI.jsx
@@ -1,9 +1,15 @@
+import { atom, useAtom } from "jotai";
+
+export const currentPageAtom = atom("intro");
+
 export const UI = () => {
+  const [currentPage, setCurrentPage] = useAtom(currentPageAtom);
   return (
     <div className="fixed inset-0 pointer-events-none">
       <section
-        className={`flex w-full h-full flex-col items-center justify-center durration-500 
-        ${currentPage === "home" ? "" : "opacity-0"}`}
+        className={`flex w-full h-full flex-col items-center justify-center 
+      duration-500
+      ${currentPage === "home" ? "" : "opacity-0"}`}
       >
         <div className="h-[66%]"></div>
         <button
